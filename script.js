ffcbeb0ac7d04557f0227470cdce37baed9246e0
JonyT24 <jony.tucek@gmail.com>
2023-05-02 18:31:56 +0200

    Vytvořena základní logika operací s dvěma čísly

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let operator = "";
+let operatorSymbol = "";
 let firstNumber = "";
 let secondNumber = "";
 let displayValue = "";
@@ -8,23 +8,36 @@ let one = document.querySelector("#one")
 
 
 
-function operate(first, second, symbol) {
-    let result;
-    if (symbol = "plus") result = first + second;
-    if (symbol = "minus") result = first - second;
-    if (symbol = "divide") result =  first / second;
-    if (symbol = "multiple") result =  first * second;
+function operate() {
+    let result = 0;
 
+    if (operatorSymbol == "plus") result = firstNumber + parseInt(document.querySelector(".screen").innerHTML);
+    if (operatorSymbol == "minus") result = firstNumber - parseInt(document.querySelector(".screen").innerHTML);
+    if (operatorSymbol == "divide") result =  firstNumber / parseInt(document.querySelector(".screen").innerHTML);
+    if (operatorSymbol == "multiply") result =  (firstNumber) * (parseInt(document.querySelector(".screen").innerHTML));
+    document.querySelector(".screen").innerHTML = result;
     return result;
+    
 };
 
-
+//Na základě kliknutého číselného tlačítka se toto číslo přidá k ostatním na obrazovce 
+//To pak tvoří celé číslo, se kterým manipulujeme ve funkci operate()
 function displayButton(button) {
     document.querySelector(".screen").innerHTML += button.value;
+
 }
 
 
 
 function clearScreen() {
     document.querySelector(".screen").innerHTML = "";
+}
+//Pokud uživatel klikne na "operator" znaménko na kalkulačce --> uloží dosud naklikanou hodnotu do firstNumber a promaže HTML v div class "scren"
+//Zároveň uloží hodnotu stisknutého znaménka do variablu "operator"
+function operator(button) {
+    firstNumber = parseInt(document.querySelector(".screen").innerHTML);
+    document.querySelector(".screen").innerHTML = "";
+    operatorSymbol = button.value
+    console.log(typeof firstNumber);
+    console.log(operatorSymbol);
 }
