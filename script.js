d7a9999107252fae2b2d1b447b6f72c25b932c3b
JonyT24 <jony.tucek@gmail.com>
2023-05-05 20:05:38 +0200

    CSS addition, previous problem with innical number 0 fixed

--- script.js
+++ script.js
@@ -3,41 +3,73 @@ let firstNumber = "";
 let secondNumber = "";
 let displayValue = "";
 
+
+
 let one = document.querySelector("#one")
 
+document.querySelector(".result-value").innerHTML = "0";
 
 
 
 function operate() {
-    let result = 0;
+    if (operatorSymbol == "+") firstNumber += parseFloat(document.querySelector(".result-value").innerHTML);
+    if (operatorSymbol == "-") firstNumber -= parseFloat(document.querySelector(".result-value").innerHTML);
+    if (operatorSymbol == "÷") firstNumber /= parseFloat(document.querySelector(".result-value").innerHTML);
+    if (operatorSymbol == "x") (firstNumber) *= parseFloat(document.querySelector(".result-value").innerHTML);
+    document.querySelector(".result-value").innerHTML = firstNumber.toFixed(2);
+    document.querySelector(".temp-value").innerHTML = "";
+    return firstNumber;
 
-    if (operatorSymbol == "plus") result = firstNumber + parseInt(document.querySelector(".screen").innerHTML);
-    if (operatorSymbol == "minus") result = firstNumber - parseInt(document.querySelector(".screen").innerHTML);
-    if (operatorSymbol == "divide") result =  firstNumber / parseInt(document.querySelector(".screen").innerHTML);
-    if (operatorSymbol == "multiply") result =  (firstNumber) * (parseInt(document.querySelector(".screen").innerHTML));
-    document.querySelector(".screen").innerHTML = result;
-    return result;
-    
 };
 
 //Na základě kliknutého číselného tlačítka se toto číslo přidá k ostatním na obrazovce 
 //To pak tvoří celé číslo, se kterým manipulujeme ve funkci operate()
 function displayButton(button) {
-    document.querySelector(".screen").innerHTML += button.value;
+    if (document.querySelector(".result-value").innerHTML === "0") {
+        document.querySelector(".result-value").innerHTML = "";
+    }
+    document.querySelector(".result-value").innerHTML += button.value;
 
 }
 
-
-
-function clearScreen() {
-    document.querySelector(".screen").innerHTML = "";
+function pointButton() {
+    if (document.querySelector(".result-value").innerHTML.includes('.')) return;
+    else document.querySelector(".result-value").innerHTML += '.';
 }
+
+
 //Pokud uživatel klikne na "operator" znaménko na kalkulačce --> uloží dosud naklikanou hodnotu do firstNumber a promaže HTML v div class "scren"
 //Zároveň uloží hodnotu stisknutého znaménka do variablu "operator"
 function operator(button) {
-    firstNumber = parseInt(document.querySelector(".screen").innerHTML);
-    document.querySelector(".screen").innerHTML = "";
+    checkForPoint();
+    firstNumber = parseFloat(document.querySelector(".result-value").innerHTML);
+    document.querySelector(".result-value").innerHTML = "0";
     operatorSymbol = button.value
-    console.log(typeof firstNumber);
-    console.log(operatorSymbol);
+    document.querySelector(".temp-value").innerHTML = firstNumber + " " + operatorSymbol;
+    
+
 }
+
+function clearScreen() {
+    document.querySelector(".result-value").innerHTML = "0";
+    document.querySelector(".temp-value").innerHTML = "";
+    
+}
+
+function deleteNumber() {
+   let string = document.querySelector(".result-value").innerHTML
+   string = string.slice(0,-1);
+   document.querySelector(".result-value").innerHTML = string;
+}
+
+function checkForPoint() {
+   let string = document.querySelector(".result-value").innerHTML
+   let result = string.includes("5")
+   
+   console.log(result);
+}
+
+//TODO 
+//Napsat funkci, která nenechá uživatele napsat více jak jednu tečku v číslu 
+
+//Zkusit udělat celá čísla tak, aby pokud není zbytek, zobrazí se pouze číslo bez dvou nul 
